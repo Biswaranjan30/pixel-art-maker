d4150d21e268b8f3d1e12009fe434a8e583ac2f3
nishtha28 <nishtha.kakkar.8@gmail.com>
2019-02-26 15:57:45 +0530

    grid with clickable pallet

---
+++ index.js
@@ -0,0 +1,38 @@
+let setColor='red'
+
+let canvasDiv = document.createElement('div');
+canvasDiv.className = 'canvas'
+
+for(let i=0;i<10;i++) {
+    let divContainer = document.createElement('div');
+    divContainer.className = 'divContainer'
+
+    for(let j=0;j<10;j++) {
+        let div = document.createElement('div');
+        div.className = 'grid';
+        div.onclick = ()=> div.style.backgroundColor =setColor;
+       // div.onmouseover = function(){
+         //   div.style.backgroundColor = 'blue';};
+        divContainer.append(div);
+    }
+    canvasDiv.append(divContainer);
+}
+document.body.append(canvasDiv);
+
+let palletDiv = document.createElement('div');
+palletDiv.className = 'pallet'
+
+let colors = ['red','blue','green'];
+let colorDiv = document.createElement('div');
+colorDiv.className = 'colorDiv';
+
+colors.forEach(function(element) {
+    let item = document.createElement('div');
+    item.style.backgroundColor = element;
+    item.className = 'item';
+    item.onclick=()=> setColor= element;
+     colorDiv.append(item);
+});
+
+palletDiv.append(colorDiv);
+document.body.append(palletDiv);
